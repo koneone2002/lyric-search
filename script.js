c2493770090fba960445c663e448c0e5b2a25266
koneone2002 <katieo3507@gmail.com>
2020-05-10 12:55:08 -0700

    first js commit

---
+++ script.js
@@ -0,0 +1,33 @@
+const form = document.getElementById('form');
+const search = document.getElementById('search');
+const result = document.getElementById('result');
+const more = document.getElementById('more');
+
+const apiURL = 'https://api.lyrics.ovh';
+
+// Search by song or artist
+// function searchSongs(term) {
+//   fetch(`${apiURL}/suggest/${term}`)
+//     .then(res => res.json())
+//     .then(data => console.log(data));
+// }
+
+async function searchSongs(term) {
+  const res = await fetch(`${apiURL}/suggest/${term}`);
+  const data = await res.json();
+  showData(data);
+}
+
+// Show song and artist in DOM
+function showData(data) {}
+// Event Listeners
+form.addEventListener('submit', e => {
+  e.preventDefault();
+
+  const searchTerm = search.value.trim();
+  if (!searchTerm) {
+    alert('Please type in a search term');
+  } else {
+    searchSongs(searchTerm);
+  }
+});
